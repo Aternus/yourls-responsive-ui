bb1af91545138f9257984a06c77dea4870da1d93
Aternus <kiril@atern.us>
2026-01-05 11:31:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# yourls-responsive-ui
+
+Responsive UI makes your YOURLS admin pretty ✨.
+
+It is inspired by the [Sleeky](https://github.com/Flynntes/Sleeky) project and
+supports Mobile, Tablet and Desktop.
+
+## Installation
+
+1. Copy or `git clone https://github.com/Aternus/yourls-responsive-ui.git` the
+   project to `/user/plugins`.
+2. Go to the Admin → Manage Plugins and activate the `Responsive UI` plugin.
+3. You should see the new UI at `https://your-yourls-instance.com/admin`.
+
+## License
+
+MIT license
+
+## Contact
+
+Website: https://atern.us
---
+++ plugin.php
@@ -0,0 +1,168 @@
+<?php
+/*
+    Plugin Name: Responsive UI
+    Plugin URI: https://github.com/Aternus/yourls-responsive-ui
+    Description: Responsive UI for your YOURLS admin
+    Version: 1.0.0
+    Author: Kiril Reznik
+    Author URI: https://atern.us/
+*/
+
+// No direct call
+if ( ! defined( 'YOURLS_ABSPATH' ) ) {
+    die();
+}
+
+/*
+ * HTML Head
+ *********************************************************/
+
+function responsive_init() {
+    echo <<<HEAD
+        <style>body {background: unset;}</style>
+        HEAD;
+}
+
+yourls_add_action( 'html_head', 'responsive_init' );
+
+function setTheme( $theme ) {
+    $url = yourls_plugin_url( __DIR__ );
+    if ( $theme == "light" ) {
+        echo <<<HEAD
+            <link rel="stylesheet" href="$url/assets/css/light.css">
+            <link rel="stylesheet" href="$url/assets/css/animate.min.css">
+            <script src="$url/assets/js/theme.js"></script>
+            <meta name="responsive_theme" content="light">
+            HEAD;
+    } elseif ( $theme == "dark" ) {
+        echo <<<HEAD
+            <link rel="stylesheet" href="$url/assets/css/dark.css">
+            <link rel="stylesheet" href="$url/assets/css/animate.min.css">
+            <script src="$url/assets/js/theme.js"></script>
+            <meta name="responsive_theme" content="dark">
+            HEAD;
+    }
+}
+
+function responsive_head_scripts() {
+    // This is so the user doesn't have to reload page twice in settings screen
+    if ( isset( $_POST['theme_choice'] ) ) {
+        // User has just changed theme
+        if ( $_POST['theme_choice'] == "light" ) {
+            setTheme( "light" );
+        } else {
+            setTheme( "dark" );
+        }
+    } else {
+        // User has not just changed theme
+        if ( yourls_get_option( 'theme_choice' ) == "light" ) {
+            setTheme( "light" );
+        } else {
+            setTheme( "dark" );
+        }
+    }
+}
+
+yourls_add_action( 'html_head', 'responsive_head_scripts' );
+
+
+function responsive_head_plugin_url() {
+    $url = yourls_plugin_url( __DIR__ );
+    echo <<<HEAD
+        <meta name="pluginURL" content="$url">
+        HEAD;
+}
+
+yourls_add_action( 'html_head', 'responsive_head_plugin_url' );
+
+
+/*
+ * Plugins Loaded
+ *********************************************************/
+
+function responsive_settings_update() {
+    $in = $_POST['theme_choice'];
+
+    if ( $in ) {
+        if ( $in == "light" or $in == "dark" ) {
+            // Update value in database
+            yourls_update_option( 'theme_choice', $in );
+        } else {
+            echo "Error";
+        }
+    }
+}
+
+function responsive_settings_handler() {
+    // Check if a form was submitted
+    if ( isset( $_POST['theme_choice'] ) ) {
+        // Check nonce
+        yourls_verify_nonce( 'responsive_settings' );
+
+        // Process form
+        responsive_settings_update();
+    }
+
+    // Get value from database
+    $theme_choice = yourls_get_option( 'theme_choice' );
+
+    // Create nonce
+    $nonce = yourls_create_nonce( 'responsive_settings' );
+
+    echo <<<HTML
+        <main>
+        	<h2>Responsive UI Settings</h2>
+        	<form method="post">
+        	<input type="hidden" name="nonce" value="$nonce" />
+        	<p>
+        		<label>Theme</label>
+        		<select name="theme_choice" size="1" id="ui_selector">
+        			<option value="dark" <?= $theme_choice === 'dark' ? 'selected' : ''; ?>>Dark</option>
+        			<option value="light" <?= $theme_choice === 'light' ? 'selected' : ''; ?>>Light</option>
+        		</select>
+        	</p>
+        	<p><input type="submit" value="Save" class="button" /></p>
+        	</form>
+        </main>
+        HTML;
+}
+
+function responsive_settings() {
+    yourls_register_plugin_page( 'responsive_settings',
+        'Responsive UI Settings', 'responsive_settings_handler' );
+    // parameters: page slug, page title, and function that will display the page itself
+}
+
+yourls_add_action( 'plugins_loaded', 'responsive_settings' );
+
+
+/*
+ * Filters
+ *********************************************************/
+
+function responsive_admin_links( $admin_links ) {
+    if ( true !== yourls_is_valid_user() ) {
+        $admin_links = [];
+    } else {
+        $admin_links['help'] = [
+            'url'    => yourls_site_url( false, '/' ) . '/readme.html',
+            'anchor' => yourls__( 'Help' ),
+        ];
+    }
+
+    $admin_links['frontend'] = [
+        'url'    => yourls_site_url( false, '/' ),
+        'anchor' => '<i class="material-icons">arrow_back</i> ' . yourls__( 'Frontend Interface' ),
+    ];
+
+    return $admin_links;
+}
+
+yourls_add_filter( 'admin_links', 'responsive_admin_links' );
+
+
+function responsive_help_link( $help_link ) {
+    return null;
+}
+
+yourls_add_filter( 'help_link', 'responsive_help_link' );
